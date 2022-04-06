78495fcea2eff720968c0f05a57da2334d2ad0c9
Abdelaty Abdallah <abdelaty.magdi@gmail.com>
2022-04-06 12:00:07 +0200

    [REFACTOR] Remove logs.

--- App/services/news/news_api.js
+++ App/services/news/news_api.js
@@ -2,8 +2,6 @@
 import { FETCH_SUCCESS, FETCH_FAILURE, FETCHING, API_KEY } from '../../utils/constants';
 
 export function fetchNews() {
-  console.log('fetchNews');
-    console.log('dispatchfetchNews');
     return fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + API_KEY)
       .then(res => res.json())
       .then(json => {
@@ -15,8 +13,6 @@ export function fetchNews() {
 }
 
 function getNewsSuccess(data) {
-  console.log('getNewsSuccess');
-
   return {
     type: FETCH_SUCCESS,
     data,
@@ -24,8 +20,6 @@ function getNewsSuccess(data) {
 }
 
 function getNewsFailure() {
-  console.log('getNewsFailure');
-
   return {
     type: FETCH_FAILURE,
   };
