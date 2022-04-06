e009c4dfe5e7eb670236c67958e8c040b82f89e0
Abdelaty Abdallah <Abdelaty.magdi@gmail.com>
2022-04-06 22:48:38 +0200

    [INTEGRATION] i18n: change news by user language

---
+++ App/config/language.js
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import i18n from '../assets/i18n/i18n';
+
+export const getCurrentLanguage = () => {
+    return i18n.language;
+};
--- App/services/news/news_api.js
+++ App/services/news/news_api.js
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { FETCH_SUCCESS, FETCH_FAILURE, FETCHING, API_KEY } from '../../utils/constants';
+import { FETCH_SUCCESS, FETCH_FAILURE, API_KEY } from '../../utils/constants';
+import { getCurrentLanguage } from '../../config/language';
 
 export function fetchNews() {
-    return fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + API_KEY)
-      .then(res => res.json())
-      .then(json => {
-        return getNewsSuccess(json.articles);
-      })
-      .catch(err => getNewsFailure(err));
+  return fetch('https://newsapi.org/v2/top-headlines?language=' + getCurrentLanguage() + '&apiKey=' + API_KEY)
+    .then(res => res.json())
+    .then(json => {
+      return getNewsSuccess(json.articles);
+    })
+    .catch(err => getNewsFailure(err));
 
 
 }
