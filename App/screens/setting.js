4a8c71ec162f4ccadbda4b767edbd6b09f95c443
Abdelaty Abdallah <Abdelaty.magdi@gmail.com>
2022-04-07 08:47:20 +0200

    [REFACTOR] RTL: remove current integration for unstability.

--- App/screens/setting.js
+++ App/screens/setting.js
@@ -1,5 +1,5 @@
-/* eslint-disable prettier/prettier 
 /* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
 import * as React from 'react';
 import '../assets/i18n/i18n';
 
@@ -8,12 +8,7 @@ import { View, Button } from 'react-native';
 import { AppConsumer } from '../app_context_provider';
 import { useTranslation } from 'react-i18next';
 import { LightTheme, DarkTheme } from '../styles/themes';
-import { I18nManager } from 'react-native';
-import RNRestart from 'react-native-restart';
-
-
-
-
+// import { I18nManager } from 'react-native';
 export const SettingsScreen = () => {
     const { t, i18n } = useTranslation();
 
@@ -30,12 +25,19 @@ export const SettingsScreen = () => {
         <AppConsumer>
             {appConsumer => (
                 <>
-                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: appConsumer.theme.colors.primary }}>
-
+                    <View
+                        style={{
+                            flex: 1,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            backgroundColor: appConsumer.theme.colors.primary,
+                        }}>
                         <Button
-                            title="Switch Theme"
+                            title={t('change_theme')}
                             onPress={() => {
-                                appConsumer.updateTheme(appConsumer.theme === LightTheme ? DarkTheme : LightTheme);
+                                appConsumer.updateTheme(
+                                    appConsumer.theme === LightTheme ? DarkTheme : LightTheme,
+                                );
                             }}
                         />
 
@@ -43,21 +45,22 @@ export const SettingsScreen = () => {
                             title={t('change_language')}
                             onPress={() => {
                                 changeLanguage(currentLanguage === 'en' ? 'ar' : 'en');
-                                if (currentLanguage === 'ar') {
-                                    I18nManager.forceRTL(true); 
-                                    console.log('now arabic ' + I18nManager.isRTL);
-                                    RNRestart.Restart();
-                                }
-                                else {
-                                    I18nManager.forceRTL(false);
-                                    console.log('now english ' + I18nManager.isRTL);
-                                    RNRestart.Restart();
-                                }
-                                RNRestart.Restart();
-                                console.log(I18nManager.isRTL);
+                                // I18nManager.forceRTL(true);
+                                // RNRestart.Restart();
+                                // I18nManager.forceRTL(true);
+                                // console.log('now arabic ' + I18nManager.isRTL);
+                                // } else {
+                                //   I18nManager.forceRTL(false);
+                                //   console.log('now english ' + I18nManager.isRTL);
+                                //   RNRestart.Restart();
+                                // }
+                                // RNRestart.Restart();
+                                // console.log(I18nManager.isRTL);
                             }}
                         />
-                    </View></>)}
+                    </View>
+                </>
+            )}
         </AppConsumer>
     );
-}
+};
